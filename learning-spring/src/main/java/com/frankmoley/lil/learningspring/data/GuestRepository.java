69dd245f338a3fd00e1213fbcd8b9cc7b3b0e164
Jeremy_2 <Jeremy_2@Solitude.cable.rcn.com>
2023-02-23 13:43:51 -0600

    This is the initial commit

---
+++ learning-spring/src/main/java/com/frankmoley/lil/learningspring/data/GuestRepository.java
@@ -0,0 +1,11 @@
+package com.frankmoley.lil.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long>{
+
+	
+
+}
